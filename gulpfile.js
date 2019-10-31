da283e3bbf4a983b8af9553a2c3b66133ec9f310
bhadresharya <bhadresharya@hotmail.com>
2019-10-31 19:49:14 +0530

    added gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var autoprefixer = require('gulp-autoprefixer');
 var shorthand = require('gulp-shorthand');
 var cleancss = require('gulp-clean-css');
 var mediaQ = require('gulp-group-css-media-queries');
-var rename = require('gulp-rename');
+var concat = require('gulp-concat');
 
 gulp.task('style', function () {
     return gulp.src('./scss/**/*.scss', { sourcemaps: true })
@@ -14,15 +14,17 @@ gulp.task('style', function () {
         .pipe(gulp.dest('./css',  { sourcemaps: '.' }));
 });
 gulp.task('optimize', function () {
-    return gulp.src('./scss/**/*.scss', { sourcemaps: true })
+    return gulp.src(
+        [
+            './scss/style.scss',
+            './scss/responsive.scss'
+        ], { sourcemaps: true })
         .pipe(scss().on('error', scss.logError))
         .pipe(autoprefixer())
         .pipe(shorthand())
         .pipe(mediaQ())
         .pipe(cleancss())
-        .pipe(rename({
-            suffix: ".min"
-        }))
+        .pipe(concat("master.min.css"))
         .pipe(gulp.dest('./css', { sourcemaps: '.' }));
 });
 gulp.task('default', function () {
